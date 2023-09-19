bc0f1f01e40a4c1718401a67d597728800b7b40d
Amit Subhash Chejara <amitsubhashchejara@gmail.com>
2023-09-19 10:59:25 +0530

    first try

--- DocAI.py
+++ DocAI.py
@@ -112,7 +112,7 @@ async def start(update: Update, context: CallbackContext):
     #     response=get_completion_from_messages(messages, temperature=1)
     #     await context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-# Handle Document input
+# Handle Document input try
 
 async def doc_Inputs(update: Update, context: CallbackContext):
     #loading the document
