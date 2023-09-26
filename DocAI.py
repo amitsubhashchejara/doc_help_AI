44b8c633cdbcfbea626a55d0c5e3734413f87bbd
Amit Subhash Chejara <amitsubhashchejara@gmail.com>
2023-09-26 21:58:37 +0530

    Final chages to the PDF summarization version of DocHelpAI Bot.

--- DocAI.py
+++ DocAI.py
@@ -20,8 +20,9 @@ import openai
 
 # all the langchain modules
 import langchain
+from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import TelegramChatFileLoader, TelegramChatApiLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain.chains import LLMChain
@@ -41,8 +42,8 @@ openai.api_key = os.getenv("api_key_openai_1")
 TOKEN = os.getenv("T_bot_api_2")
 
 
-# llm_name="gpt-3.5-turbo"
-# llm=ChatOpenAI(model_name=llm_name, temperature=0, openai_api_key=os.getenv('api_key_openai_1'))
+llm_name="gpt-3.5-turbo"
+llm=ChatOpenAI(model_name=llm_name, temperature=0, openai_api_key=os.getenv('api_key_openai_1'))
 
 BOT_ID = 6596826073
 
@@ -75,9 +76,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             "Please click on menu button and select /doc_help to\nget help related to document or just chat with me\nin general here."
         )
     else:
-        await update.message.reply_text(
-            "Welcome back! Please click on menu button and select /doc_help to\nget help related to document or just chat with me\nin general here."
-        )
+        chat_query = update.message.text
+        prompt="""
+                You are a friendly Document Assistant. Your job is to help users find specific information from their documents or summarize them. You will take the document file as an input and output the required content in a structured format. You can communicate with the user, but strictly only regarding the files they have given as input and no other than that. Please follow these instructions to fulfill your role:
+                - Ask the user to click on /doc_help command from the menu button and to upload their document and specify the type of document it is (PDF, Excel, Word, PPT, etc.)
+                - Ask the user what information they are looking for or if they would like you to summarize the document for them
+                - Provide the user with the information they need in a structured format such as JSON or HTML
+                - Check whether all conditions are satisfied before providing the output and make sure that you assist with doucment related task only, say no to questions related to any other topic except document related questions.
+                - Strictly only assist the user with the documents related task, the documents that they have provided as input and do not provide information on any other topics
+                Thank you for your assistance!"""
+        messages=[  
+            {'role':'system', 'content':prompt},    
+            {'role':'user', 'content':chat_query}  ]
+
+        chat_result=get_completion_from_messages(messages, temperature=0.5)
+        await update.message.reply_text(chat_result)
 
 
 async def doc_help(update: Update, _: ContextTypes.DEFAULT_TYPE):
@@ -89,13 +102,47 @@ async def doc_help(update: Update, _: ContextTypes.DEFAULT_TYPE):
 
 async def chat_with_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.user_data.get("document_uploaded", False):
-        await update.message.reply_text(
-            "Please upload a document first, use /doc_help."
-        )
+        chat_query = update.message.text
+        prompt="""
+                You are a friendly Document Assistant. Your job is to help users find specific information from their documents or summarize them. You will take the document file as an input and output the required content in a structured format. You can communicate with the user, but strictly only regarding the files they have given as input and no other than that. Please follow these instructions to fulfill your role:
+                - Ask the user to click on /doc_help command from the menu button and to upload their document and specify the type of document it is (PDF, Excel, Word, PPT, etc.)
+                - Ask the user what information they are looking for or if they would like you to summarize the document for them
+                - Provide the user with the information they need in a structured format such as JSON or HTML
+                - Check whether all conditions are satisfied before providing the output and make sure that you assist with doucment related task only, say no to questions related to any other topic except document related questions.
+                - Strictly only assist the user with the documents related task, the documents that they have provided as input and do not provide information on any other topics
+                Thank you for your assistance!"""
+        messages=[  
+            {'role':'system', 'content':prompt},    
+            {'role':'user', 'content':chat_query}  ]
+
+        chat_result=get_completion_from_messages(messages, temperature=0.5)
+        await update.message.reply_text(chat_result)
         return
 
     text = update.effective_message.text
     # TODO: Process the text here:
+    # Build prompt
+    
+    template = """Use the following pieces of context to answer the question at the end. Make sure that you 
+    only answer the questions which are related to the document provided to you.If the question is out of the 
+    context or out of the topic like other than the document related task, just say that please ask questions related to this doucment. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=db.as_retriever(),
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+    )
+
+    question = text
+    result = qa_chain({"query": question})
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=result['result'])
 
 
 async def download_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -106,25 +153,28 @@ async def download_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     # ------------------
     # TODO: Process the document here:
-    await process_and_load_pdf(file)
+    await process_and_load_pdf(update, context, file)
 
     context.user_data["document_uploaded"] = True
-    await update.effective_message.reply_text(
-        "Document processed successfully!!!\nYou can ask me questions related to this document now."
-    )
+    
 
 
 async def process_and_load_pdf(
     update: Update, context: ContextTypes.DEFAULT_TYPE, pdf_file_path: pathlib.Path
 ):
-    loader = TelegramChatFileLoader(user_input_doc)
-    pages = loader.load()
+    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Processing the document...",
+    )
+    loader=PyPDFLoader(str(pdf_file_path))
+    pages=loader.load()
 
     # splitting the document
-    r_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=300, separators=["\n\n", "\n", " ", ""]
+    c_splitter = CharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=300, separator=" "
     )
-    splitted_pages = r_splitter.split(pages)
+    splitted_pages = c_splitter.split_documents(pages)
 
     # embedding the document
     embedding = OpenAIEmbeddings(openai_api_key=openai.api_key)
@@ -141,74 +191,6 @@ async def process_and_load_pdf(
         chat_id=update.effective_chat.id,
         text="Ask me questions related to this document",
     )
-    user_input_4 = update.message.text
-
-    # Build prompt
-    template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
-    {context}
-    Question: {question}
-    Helpful Answer:"""
-    QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
-
-    qa_chain = RetrievalQA.from_chain_type(
-        llm,
-        retriever=db.as_retriever(),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-    )
-
-    question = user_input_4
-    result = qa_chain({"query": question})
-    context.bot.send_message(chat_id=update.effective_chat.id, text=result)
-
-
-# def doc_help(update: Update, context: CallbackContext):
-#     user = update.effective_user  # Get the user object
-#     user_id = user.id
-#     #loading the document
-#     user_input = update.message
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="hi you are in doc help function")
-#     if user_input.document:
-#         context.bot.send_message(chat_id=update.effective_chat.id, text="Processing the document...")
-#         loader = TelegramChatFileLoader(user_input)
-#         pages=loader.load()
-
-#         #splitting the document
-#         r_splitter=RecursiveCharacterTextSplitter(
-#         chunk_size=1000,
-#         chunk_overlap=300,
-#         separators=["\n\n", "\n", " ", ""]
-#         )
-#         splitted_pages=r_splitter.split(pages)
-
-#         #embedding the document
-#         embedding=OpenAIEmbeddings(openai_api_key=openai.api_key)
-#         global db
-#         db=DocArrayInMemorySearch.from_documents(
-#         splitted_pages,
-#         embedding=embedding,
-#         )
-#         context.bot.send_message(chat_id=update.effective_chat.id, text="Document processed successfully!!!\nYou can ask me questions related to this document now.")
-
-#         user_input=update.message
-#         if user_input.text:
-#             # Build prompt
-#             template="""Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer.
-#             {context}
-#             Question: {question}
-#             Helpful Answer:"""
-#             QA_CHAIN_PROMPT=PromptTemplate.from_template(template)
-
-#             qa_chain=RetrievalQA.from_chain_type(
-#             llm,
-#             retriever=db.as_retriever(),
-#             return_source_documents=True,
-#             chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
-#             )
-
-#             question=update.message.text
-#             result=qa_chain({"query": question})
-#             context.bot.send_message(chat_id=update.effective_chat.id, text=result)
 
 
 def main():
